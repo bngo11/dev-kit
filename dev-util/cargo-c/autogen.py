a2a1ff0349d15c77999e9c85fd49133aa20787da
shadow <shadowngoz@gmail.com>
2025-03-19 16:20:16 -0500

    Autogenerated tree updates.
    merged:
    
      flora: 523a446b976b43c4ede7fd49048643aaf4812e42
      faustoo: 962b77da4f92c74fa70933968b2d5b88766b0ca9
      gentoo-staging: 9292583023a6610905c79a4707db3ac6c63b3a31
      kit-fixups: f57925df21b513e03885e488fc499b96353d98b0

---
+++ dev-util/cargo-c/autogen.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import json
+
+async def generate(hub, **pkginfo):
+	github_user = "lu-zero"
+	github_repo = "cargo-c"
+	json_data = await hub.pkgtools.fetch.get_page(f"https://api.github.com/repos/{github_user}/{github_repo}/releases", is_json=True)
+	version = None
+	url = None
+
+	for item in json_data:
+		try:
+			if item["prerelease"] or item["draft"]:
+				continue
+
+			version = item["tag_name"].strip('v')
+			list(map(int, version.split(".")))
+
+			for asset in item['assets']:
+				asset_name = asset["name"]
+				url = item["tarball_url"]
+				break
+
+			if url:
+				break
+
+		except (KeyError, IndexError, ValueError):
+			continue
+
+	if version and url:
+		final_name=f"{asset_name}-{version}.tar.gz"
+		src_artifact = hub.pkgtools.ebuild.Artifact(url=url, final_name=final_name)
+		artifacts = await hub.pkgtools.rust.generate_crates_from_artifact(src_artifact)
+
+		ebuild = hub.pkgtools.ebuild.BreezyBuild(
+			**pkginfo,
+			version=version,
+			github_user=github_user,
+			github_repo=github_repo,
+			crates=artifacts['crates'],
+			artifacts=[
+				src_artifact,
+				*artifacts['crates_artifacts'],
+			],
+		)
+		ebuild.push()
+
+
+# vim: ts=4 sw=4 noet
