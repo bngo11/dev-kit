9f278d1dd5d274d0b6c978e97f160e6262d9be00
shadow <shadowngoz@gmail.com>
2025-09-30 17:21:07 -0500

    Autogenerated tree updates.
    merged:
    
      flora: 523a446b976b43c4ede7fd49048643aaf4812e42
      faustoo: 962b77da4f92c74fa70933968b2d5b88766b0ca9
      gentoo-staging: 9292583023a6610905c79a4707db3ac6c63b3a31
      kit-fixups: 7cb390fbdc6ab93f6ba6f6ae86c612f79bd29d67

--- dev-cpp/eigen/autogen.py
+++ dev-cpp/eigen/autogen.py
@@ -1,26 +1,52 @@
 #!/usr/bin/env python3
 
+import json
 
 async def generate(hub, **pkginfo):
-	user = "libeigen"
-	repo = pkginfo["name"]
-	project_path = f"{user}%2F{repo}"
-	releases_data = await hub.pkgtools.fetch.get_page(
-		f"https://gitlab.com/api/v4/projects/{project_path}/releases", is_json=True
-	)
-	for release in releases_data:
-		if release["tag_name"].upper().isupper():
+	gitlabid = 15462818
+	gitlaburl = "gitlab.com"
+	json_data = await hub.pkgtools.fetch.get_page(f"https://{gitlaburl}/api/v4/projects/{gitlabid}/releases", is_json=True)
+	version = None
+	url = None
+	basever = "5.0"
+
+	for item in json_data:
+		try:
+			version = item['tag_name']
+			verlist = version.split(".")
+			list(map(int, verlist))
+			if len(verlist) > 1:
+				if int(verlist[1]) >= 89 and int(verlist[0]) != 0:
+					continue
+
+			if basever:
+				baselist = basever.split('.')
+				baselen = len(baselist)
+				if verlist[:baselen] != baselist:
+					continue
+
+			sources = item['assets']['sources']
+			sources.reverse()
+
+			for src in sources:
+				if src['format'] in ['tar.bz2', 'tar.gz', 'tar.xz']:
+					url = src['url']
+					break
+
+			if version and url:
+				break
+
+		except (IndexError, ValueError, KeyError):
 			continue
-		version = release["tag_name"]
-		break
+	else:
+		version = None
 
-	url = f"https://gitlab.com/{user}/{repo}/-/archive/{version}/{repo}-{version}.tar.bz2"
-	ebuild = hub.pkgtools.ebuild.BreezyBuild(
-		**pkginfo,
-		version=version,
-		artifacts=[hub.pkgtools.ebuild.Artifact(url=url)],
-	)
-	ebuild.push()
-
-
+	if version and url:
+		pkginfo['version'] = version
+		final_name = f'{pkginfo["name"]}-{version}.{".".join(url.split(".")[-2:])}'
+		ebuild = hub.pkgtools.ebuild.BreezyBuild(
+			**pkginfo,
+			artifacts=[hub.pkgtools.ebuild.Artifact(url=url, final_name=final_name)]
+		)
+		ebuild.push()
 # vim: ts=4 sw=4 noet
